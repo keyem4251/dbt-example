772e7a0fc8da65ae66691aa8607dac98ac31acc4
keyem4251@gmail.com <keyem4251@gmail.com>
2022-12-11 03:13:12 +0000

    refactor for loop

--- models/order_payment_method_amounts.sql
+++ models/order_payment_method_amounts.sql
@@ -1,10 +1,8 @@
 select
     order_id,
-    sum(
-        case when payment_method = 'bank_transfer' then amount end
-    ) as bank_transfer_amount,
-    sum(case when payment_method = 'credit_card' then amount end) as credit_card_amount,
-    sum(case when payment_method = 'gift_card' then amount end) as gift_card_amount,
+    {% for payment_method in ["bank_transafer", "credit_card", "gift_card"] %}
+    sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount,
+    {% endfor %}
     sum(amount) as total_amount
 from {{ ref("raw_payments") }}
 group by 1
