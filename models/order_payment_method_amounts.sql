eacd03bee789107cb45b0ef3ce62493faddeb4f9
keyem4251@gmail.com <keyem4251@gmail.com>
2022-12-12 12:08:25 +0000

    use white space control

--- models/order_payment_method_amounts.sql
+++ models/order_payment_method_amounts.sql
@@ -1,10 +1,10 @@
-{% set payment_methods = ["bank_transafer", "credit_card", "gift_card"] %}
+{%- set payment_methods = ["bank_transafer", "credit_card", "gift_card"] -%}
 
 select
     order_id,
-    {% for payment_method in payment_methods %}
-    sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount,
-    {% if not loop.last %},{% endif %}
+    {%- for payment_method in payment_methods %}
+    sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount
+    {%- if not loop.last %},{% endif -%}
     {% endfor %}
 from {{ ref("raw_payments") }}
 group by 1
