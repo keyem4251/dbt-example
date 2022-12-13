559590cfc4bb3d1c2070da867786523ad330d2b7
keyem4251@gmail.com <keyem4251@gmail.com>
2022-12-13 11:38:11 +0000

    dynamically retrieve the list of payment methods

--- macros/get_payment_methods.sql
+++ macros/get_payment_methods.sql
@@ -1,3 +1,20 @@
 {% macro get_payment_methods() %}
-{{ return(["bank_transafer", "credit_card", "gift_card"]) }}
+
+{% set payment_methods_query %}
+select distinct
+payment_methods_query
+from app_data.payments
+order by 1
+{% endset %}
+
+{% set results = run_query(payment_methods_query) %}
+
+{% if execute %}
+{% set results_list = results.columns[0].values() %}
+{% else %}
+{% set results_list = [] %}
+{% endif %}
+
+{{ return(results_list) }}
+
 {% endmacro %}
